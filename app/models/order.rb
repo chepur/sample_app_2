af2006fa27ec3a677be5eb7d233584f92f3ad838
chepur <chepurev@gmail..com>
2016-06-07 23:17:27 +0300

    changed model validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,5 @@ class Order < ActiveRecord::Base
   validates :name, presence: true, length: {minimum: 4, maximum: 30}
   validates :surname, presence: true, length: {minimum: 4, maximum: 30}
   validates :tz, presence: true, length: {is: 9}
-  validates :phone, presence: true, :numericality => true,
-            length: {minimum: 10, maximum: 15}
+  validates :phone, presence: true, length: {minimum: 10, maximum: 15}
 end
