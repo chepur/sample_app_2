c06854348690f022f17acbf6d298e832bf30151b
chepur <chepurev@gmail.com>
2016-10-03 13:56:39 +0300

    finished nested form, bad validation in models

---
+++ app/assets/javascripts/simcards.js
@@ -0,0 +1,25 @@
+$(document).on('ready page:load', function () {
+
+    //function for handling nested simcard fields on order form
+    var simcardsDisp = function () {
+        if ($('.simcard_field .fields:not([style*="display: none"])').length == 1) {
+            $('.simcard_field .fields .form-group').css('width', '100%');
+            $('.icon-remove').hide()
+        }
+        else {
+            $('.simcard_field .fields .form-group').css('width', '95%');
+            $('.icon-remove').show()
+        }
+    };
+    simcardsDisp();
+
+    $(document).on('nested:fieldAdded', function (event) {
+        simcardsDisp();
+    });
+    $(document).on('nested:fieldRemoved', function (event) {
+        simcardsDisp();
+    });
+});
+
+
+
